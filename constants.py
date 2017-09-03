8709faa57c3d209a739c5d63be24d29916b66b22
gsproston <gsproston94@gmail.com>
2017-09-03 22:43:13 +0100

    moved ratios and resolutions to the constants file

--- constants.py
+++ constants.py
@@ -9,3 +9,10 @@ else:
   TURNTIME = 1 # how long the turns take in seconds, must be > 1
   AIMINCALLTIME = 0.5 # min time before the AI can call snap
 SUITS = ["diamonds","clubs","hearts","spades"]
+
+# window constants
+RATIOS = ["16:9","16:10","4:3"] # ratios
+RES169 = [[1024,576],[1152,648],[1280,720],[1366,768],[1600,900],[1920,1080]]
+RES1610 = [[1280,800],[1440,900],[1680,1050]]
+RES43 = [[960,720],[1024,768],[1280,960],[1400,1050],[1440,1080],[1600,1200],[1856,1392]]
+RESS = [RES169,RES1610,RES43] # holds various resolution options
--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+import constants as cs
 
 # new flags set
 # returns the new screen object
@@ -21,8 +22,15 @@ def resetFlags(fscreen,bwindow):
     else:
       flags = 0
   pygame.display.set_mode((w,h),flags)
+  
+def getRatio():
+  return cs.RATIOS[ratind]
+  
+def getResolution():
+  return cs.RESS[ratind][resind]
 
 def init():
+  global ratind, resind
   print("Initialising the display")
   # variables
   # deciding on a square window for this card game
@@ -32,14 +40,9 @@ def init():
   # menu variables
   fscreen = False # fullscreen
   bwindow = False # borderless
-  RATIOS = ["16:9","16:10","4:3"] # ratios
-  RESS = [[],[],[]] # holds various resolution options
-  RES169 = [[1024,576],[1152,648],[1280,720],[1366,768],[1600,900],[1920,1080]]
-  RES1610 = [[1280,800],[1440,900],[1680,1050]]
-  RES43 = [[960,720],[1024,768],[1280,960],[1400,1050],[1440,1080],[1600,1200],[1856,1392]]
-  RESS[0] = RES169
-  RESS[1] = RES1610
-  RESS[2] = RES43
+  ratind = 0 # index of the ratio menu
+  resind = 0 # index of the resolution menu
+  # ratios and resolutions can be found in constants
 
   # init screen
   screen = pygame.display.set_mode((windowWidth, windowHeight),flags)
